b09ec2c55642f521fa043751d127f5129e0bf4cf
tejaswi-kotika <tejaswi4231@gmail.com>
2025-01-29 14:32:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Smooth scrolling for Learn More button
+function scrollToAbout() {
+    const aboutSection = document.getElementById('about');
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+}
